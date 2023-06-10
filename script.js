8328abfabc7fc8e8e7df41198de70a3ba9ff7af4
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 12:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ window.addEventListener('resize', function() {
   var height = window.innerHeight;
   var sizeInfo = document.getElementById('sizeInfo');
   var h1 = sizeInfo.querySelector('h1');
-  h1.textContent = 'Width: ' + width + ' Height: ' + height;
+  h1.textContent = 'Width: ' + width +'and' + ' Height: ' + height;
 });
