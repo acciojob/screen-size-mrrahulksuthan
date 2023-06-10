c2d57249305057a9e45e610600c0b71938e1d627
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 12:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.addEventListener('resize', function() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var sizeInfo = document.getElementById('sizeInfo');
+  var h1 = sizeInfo.querySelector('h1');
+  h1.textContent = 'Width: ' + width + 'px' + ' Height: ' + height + 'px';
+});
